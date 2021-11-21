b4b16aebe636eed07d78fe9eecab4c34c240eaeb
RComunic <91998442+RComunic@users.noreply.github.com>
2021-11-20 19:04:40 -0600

    Práctica 1 - Código de Arduino 1
    Código de Arduino que se usará para generar el archivo .hex y cargarlo al circuito de Proteus

---
+++ Arduino_Led_PC.ino
@@ -0,0 +1,39 @@
+/*
+  LED PC
+  Al enviar un "1" desde la PC, el Arduino UNO 
+  detecta y enciende un LED colocado en Pin 13
+
+  * Funcionando Correctamente en REAL
+*/
+
+int led = 13; 
+char c;
+
+void setup(){
+
+Serial.begin(9600);
+pinMode(led, OUTPUT);        // Led como salida
+Serial.flush();              // Limpia el Buffer Serial de la PC
+}
+
+void loop() {  
+   if (Serial.available()>0){
+
+     c=Serial.read();
+
+     if (c== 'a'){
+        digitalWrite(led, HIGH); //Si el valor de input es 1, se enciende el led
+        Serial.println( c );    // Imprimir lectura en el monitor serial
+        Serial.println( "LED ON" );
+        
+     }
+
+     if ( c == 'b'){
+        digitalWrite(led, LOW); //Si el valor de input es diferente de 1, se apaga el LED
+        Serial.println( c );                // Imprimir en el monitor serial
+        Serial.println( "LED OFF" );
+     }
+     delay(100);
+   }
+   Serial.flush();
+}
